855615d7fd1fe7f52aa9462d4a0c5138449c91ce
Gerald Yeo <gerald@fusedthought.com>
2017-05-06 00:16:00 +0800

    added context helper

---
+++ src/lib/withContext.js
@@ -0,0 +1,35 @@
+// @flow
+import React from 'react';
+
+type Props = {
+  formData: Object;
+  formMeta: Object;
+};
+
+type ChildContextType = {
+  formData: Object;
+  formMeta: Object;
+}
+
+const withContext = () => (Component: RComponent): RComponent => {
+
+  class ComponentWithContext extends React.Component {
+    props: Props
+    childContextTypes: ChildContextType
+
+    getChildContext = (): ChildContextType => ({
+      formData: this.props.formData,
+      formMeta: this.props.formMeta
+    })
+
+    render() {
+      return (
+        <Component {...this.props} />
+      );
+    }
+  }
+
+  return ComponentWithContext;
+};
+
+export default withContext;
