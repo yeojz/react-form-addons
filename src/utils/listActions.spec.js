ace3935eded1d9a867daa0c90ad2e361e79063b0
Gerald Yeo <gerald@fusedthought.com>
2017-05-08 00:39:51 +0800

    :white_check_mark: listAction.add coverage

--- src/utils/listActions.spec.js
+++ src/utils/listActions.spec.js
@@ -5,18 +5,49 @@ describe('utils/listActions', function () {
   it('should generate add before action', function () {
     const props = {
       formData: {
-        test: [{}]
+        test: [{a: 1}]
       },
       formMeta: {
-        test: [{}, {}]
+        test: [{a: 1}, {b: 2}]
       }
     }
     const evt = new SyntheticFormEvent();
     const result = listActions.add('before', 'test', props, evt);
     expect(result.formData.test.length).toBe(2);
     expect(result.formMeta.test.length).toBe(3);
+    expect(Object.keys(result.formData.test[0]).length).toBe(0);
+    expect(Object.keys(result.formData.test[1]).length).toBe(1);
     expect(result.target.name).toBe('test');
     expect(result.target.value).toBe(result.formData.test);
   });
 
+  it('should generate add after action', function () {
+    const props = {
+      formData: {
+        test: [{a: 1}]
+      },
+      formMeta: {
+        test: [{a: 1}, {b: 2}]
+      }
+    }
+    const evt = new SyntheticFormEvent();
+    const result = listActions.add('after', 'test', props, evt);
+    expect(result.formData.test.length).toBe(2);
+    expect(result.formMeta.test.length).toBe(3);
+    expect(Object.keys(result.formData.test[0]).length).toBe(1);
+    expect(Object.keys(result.formData.test[1]).length).toBe(0);
+    expect(result.target.name).toBe('test');
+    expect(result.target.value).toBe(result.formData.test);
+  });
+
+  it('should should not fail add action when no existing value', function () {
+    const props = {
+      formData: {},
+      formMeta: {}
+    }
+    const evt = new SyntheticFormEvent();
+    const result = listActions.add('before', 'test', props, evt);
+    expect(result.formData.test.length).toBe(1);
+    expect(result.formMeta.test.length).toBe(1);
+  });
 });
