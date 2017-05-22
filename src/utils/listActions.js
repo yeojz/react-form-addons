27ed4158bb18bf3a580894ee0b9205fce263f0a7
Gerald Yeo <gerald@fusedthought.com>
2017-05-22 21:21:17 +0800

    fix def

--- src/utils/listActions.js
+++ src/utils/listActions.js
@@ -42,7 +42,7 @@ const changeDelta = (name: string, idx: number) => (value: any): Object => ({
 });
 
 const change = (idx: number, name: string, props: Props, event: SyntheticFormEvent): SyntheticFormEvent => {
-  const delta: Object = changeDelta(name, idx);
+  const delta: Function = changeDelta(name, idx);
   event.formData = update(props.formData, delta(event.formData));
   event.formMeta = update(props.formMeta, delta(event.formMeta));
   event.target = getTarget(name, event.formData);
